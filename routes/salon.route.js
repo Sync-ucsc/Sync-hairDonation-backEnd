7eeb5eadc6e075e47291b3c45bd0c17623ed8128
SachinduHimash <himash1997@gmail.com>
2020-06-01 23:01:36 +0530

    Added socket io to salons

--- routes/salon.route.js
+++ routes/salon.route.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const salonRoute = express.Router();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 // Salon model
 let Salon = require('../models/salons');
 
+server.listen(3000/salon);
+
+
+
+
+// socket io
+io.on('connection', function (socket) {
+  socket.on('newdata', function (data) {
+      io.emit('new-data', { data: data });
+  });
+  socket.on('updatedata', function (data) {
+    io.emit('update-data', { data: data });
+  });
+});
+
 // Add a Salon
 salonRoute.route('/create').post((req, res, next) => {
+  const io = req.app.get('io');
+  console.log(io);
   Salon.create(req.body, (error, data) => {
     if (error) {
       return next(error)
@@ -18,6 +37,8 @@ salonRoute.route('/create').post((req, res, next) => {
 
 // Get All Salons
 salonRoute.route('/').get((req, res) => {
+  const io = req.app.get('io');
+  console.log(io);
   console.log('ss')
   Salon.find((error, data) => {
     if (error) {
@@ -31,6 +52,8 @@ salonRoute.route('/').get((req, res) => {
 
 // Get single salon
 salonRoute.route('/read/:id').get((req, res) => {
+  const io = req.app.get('io');
+  console.log(io);
   Salon.findById(req.params.id, (error, data) => {
     if (error) {
       return next(error)
@@ -43,6 +66,8 @@ salonRoute.route('/read/:id').get((req, res) => {
 
 // Update salon
 salonRoute.route('/update/:id').put((req, res, next) => {
+  const io = req.app.get('io');
+  console.log(io);
   Salon.findByIdAndUpdate(req.params.id, {
     $set: req.body
   }, (error, data) => {
@@ -58,6 +83,8 @@ salonRoute.route('/update/:id').put((req, res, next) => {
 
 // Delete salon
 salonRoute.route('/delete/:id').delete((req, res, next) => {
+  const io = req.app.get('io');
+  console.log(io);
   console.log('dd')
   Salon.findByIdAndDelete(req.params.id, (error, data) => {
     if (error) {
