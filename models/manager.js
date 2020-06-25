4a67b18acb63a78cf0dd9da9a6e4c7ed6f3ef1a2
SachinduHimash <himash1997@gmail.com>
2020-06-25 12:34:59 +0530

    added manager model and route

---
+++ models/manager.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// Define collection and schema
+const ManagerSchema = new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    telephone: {
+        type: Number
+    },
+    address: {
+        type: String
+    },
+    
+});
+
+
+
+const Manager = module.exports = mongoose.model('Manager', ManagerSchema);
+
+//Manager add
+module.exports.addManager = function (newManager, callback) {
+
+    newManager.save(callback);
+}
+
+//get all Managers
+module.exports.getAll = function (callback) {
+
+    Manager.find(callback);
+}
+
+//Manager get by id
+module.exports.getById = function (id, callback) {
+
+    Manager.findById(id, callback);
+}
+
+//update Manager
+module.exports.updateManager = function (updatedManager, callback) {
+
+    console.log(updatedManager._id)
+
+    Manager.findByIdAndUpdate(updatedManager._id, {
+        $set: updatedManager
+    }, {
+        useFindAndModify: false
+    },
+        callback);
+}
+//salon delete
+module.exports.deleteManager = function (id, callback) {
+    console.log('deleted a manager')
+    Manager.findByIdAndDelete(id, callback);
+}
---
+++ routes/manager.route.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const app = express();
+const managerRoute = express.Router();
+
+
+// Manager model
+let Manager = require('../models/manager');
+
+// Add a Manager
+managerRoute.route('/create').post((req, res, next) => {
+    const io = req.app.get('io');
+    let newManager = new Manager({
+        name: req.body.name,
+        email: req.body.email,
+        telephone: req.body.telephone,
+        address: req.body.address,
+        
+    });
+    console.log(newManager);
+
+    Manager.addManager(newManager, (err, manager) => {
+        if (err) {
+            res.json({
+                data: err,
+                success: false,
+                msg: 'Failed to add a manager'
+            })
+        } else {
+            res.json({
+                data: manager,
+                success: true,
+                msg: 'Manager Created',
+            })
+            io.emit('new-manager');
+        }
+    })
+
+});
+
+// Get All Managers
+managerRoute.route('/').get((req, res) => {
+    const io = req.app.get('io');
+    Manager.getAll((err, manager) => {
+        if (err) {
+            res.json({
+                data: '',
+                success: false,
+                msg: 'Failed to get managers'
+            })
+        } else {
+            res.json({
+                data: manager,
+                success: true,
+                msg: 'got managers',
+            })
+        }
+    })
+})
+
+
+
+// Get a single manager
+managerRoute.get('/read/:id', (req, res) => {
+    const io = req.app.get('io');
+    Manager.getById(req.params.id, (err, manager) => {
+        if (err) {
+            res.json({
+                data: '',
+                success: false,
+                msg: 'Failed to get the manager'
+            })
+        } else {
+            res.json({
+                data: manager,
+                success: true,
+                msg: 'got the manager',
+            })
+        }
+    })
+})
+
+
+// Update manager
+managerRoute.post('/update/:id', (req, res) => {
+    const io = req.app.get('io');
+    let updatedManager = Manager({
+        name: req.body.name,
+        email: req.body.email,
+        telephone: req.body.telephone,
+        address: req.body.address,
+        
+    })
+
+    Manager.updateManager(updatedManager, (err, manager) => {
+        if (err) {
+            res.json({
+                data: err,
+                success: false,
+                msg: 'Failed to update manager'
+            })
+        } else {
+            res.json({
+                data: manager,
+                success: true,
+                msg: 'updated manager',
+            })
+            io.emit('update-manager');
+
+        }
+
+
+    })
+
+
+})
+
+// Delete a manager
+managerRoute.delete('/delete/:id', (req, res) => {
+    const io = req.app.get('io');
+    Manager.deleteManager(req.params._id, (err, manager) => {
+        if (err) {
+            res.json({
+                data: err,
+                success: false,
+                msg: 'Failed to delete the manager'
+            })
+        } else {
+            res.json({
+                data: manager,
+                success: true,
+                msg: 'manager deleted',
+            })
+            io.emit('delete-manager');
+        }
+    });
+
+})
+
+module.exports = managerRoute;
