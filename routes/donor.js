fdba35236e9f16c953c8fbcd2659d00346d698b5
tharinduvindula <tharinduvindula@gmail.com>
2020-07-04 14:59:24 +0530

    aligments

--- routes/donor.js
+++ routes/donor.js
@@ -64,7 +64,7 @@ router.get('/', (req, res) => {
   
   
   // Get a single donor
-  router.get('/read/:id', (req, res) => {
+router.get('/read/:id', (req, res) => {
     const io = req.app.get('io');
     Donor.getById(req.params.id, (err,donor) => {
       if (err) {
@@ -82,10 +82,10 @@ router.get('/', (req, res) => {
         })
       }
     })
-  })
+})
 
     // Get a single donor by email
-    router.get('/getDonor/', (req, res) => {
+router.get('/getDonor/', (req, res) => {
       const io = req.app.get('io');
       Donor.getDonorByEmail(req.body.email, (err,donor) => {
         if (err) {
@@ -103,7 +103,7 @@ router.get('/', (req, res) => {
           })
         }
       })
-    })
+})
     
   
   
