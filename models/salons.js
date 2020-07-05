fb12c50aae57c506dda6757bb8bdbbdcafcfd60a
chamodi08jaya <mailtochamodij@gmail.com>
2020-07-06 03:30:00 +0530

    A

--- models/salons.js
+++ models/salons.js
@@ -36,3 +36,37 @@ const SalonSchema = new Schema({
 
 
 const Salon = module.exports = mongoose.model('Salon', SalonSchema)
+
+//salon add
+module.exports.addSalon = function (newSalon, callback) {
+
+    newSalon.save(callback);
+}
+
+//salon get all
+module.exports.getAll = function (callback) {
+
+    Salon.find(callback);
+}
+
+//salon get by id
+module.exports.getById = function (id, callback) {
+
+    Salon.findById(id, callback);
+}
+
+//update salon
+module.exports.updateSalon = function (updatedSalon, callback) {
+
+
+    Salon.findByIdAndUpdate(updatedSalon._id, {
+        $set: updatedSalon
+    }, {
+        useFindAndModify: false
+    },
+        callback);
+}
+//salon delete
+module.exports.deleteSalon = function (id, callback) {
+    Salon.findByIdAndDelete(id, callback);
+}
