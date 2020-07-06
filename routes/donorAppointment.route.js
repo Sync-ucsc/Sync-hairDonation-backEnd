e57faa525e2303dc47814697f8428aa0487e73d4
isuru1221 <isurusandaruwann1221@gmail.com>
2020-07-06 22:06:15 +0530

    fix donorAppointment

--- routes/donorAppointment.route.js
+++ routes/donorAppointment.route.js
@@ -1,104 +1,96 @@
 const express = require('express');
 const router = express.Router();
 
+const  {sendResponse} = require('../utils/response.utils');
 
 // Appointment model
 let DonorAppointment = require('../models/donorAppointment');
 
 // Add a normal customerAppointment
-router.route('/create').post((req, res, next) => {
+router.post('/create', (req, res, next) => {
     const io = req.app.get('io');
     let newDonorAppointment = new DonorAppointment({
-      SalonEmail: req.body.salonEmail,
-      DonorRequest: req.body.DonorRequest,
-      Donoremail:req.body.Donoremail,
-      customerEmail:req.body.customerEmail,
-      customerNumber:req.body.customerNumber,
-      customerName:req.body.customerName,
-      systemRequestDate:req.body.systemRequestDate,
-      appointmentDate:req.body.appointmentDate,
-      appointmentTimeSlot:req.body.appointmentTimeSlot
+        SalonEmail: req.body.salonEmail,
+        DonorRequest: req.body.DonorRequest,
+        Donoremail: req.body.Donoremail,
+        customerEmail: req.body.customerEmail,
+        customerNumber: req.body.customerNumber,
+        customerName: req.body.customerName,
+        systemRequestDate: req.body.systemRequestDate,
+        appointmentDate: req.body.appointmentDate,
+        appointmentTimeSlot: req.body.appointmentTimeSlot
     });
-    console.log(new DonorAppointment);
-    DonorAppointment.createAppointment(DonorAppointment, (err,donor ) => {
-      if (err) {
-        res.status(500);
-        res.json({
-          data: err,
-          success: false,
-          msg: 'Failed to create the appointment'
-        })
-      } else {
-        res.json({
-          data: donor,
-          success: true,
-          msg: 'Create Appointment',
-        })
-        io.emit('Create Appointment');
-      }
-    });
-  
 
-    // let user = User({
-    //   firstName: req.body.name,
-    //   lastName: '',
-    //   role: 'donor',
-    // })
-     
-    
-  });
+
+    DonorAppointment.createAppointment(newDonorAppointment, (err, donor) => {
+        if (err) {
+            res.status(500).json({
+                data: err,
+                success: false,
+                msg: 'Failed to create the appointment'
+            })
+        } else {
+            res.json({
+                data: donor,
+                success: true,
+                msg: 'Create Appointment',
+            })
+            io.emit('Create Appointment');
+        }
+    });
+});
 
 // Update Appointment
 router.post('/update/:id', (req, res) => {
     const io = req.app.get('io');
     let updatedAppointment = DonorAppointment({
-      _id: req.params.id,
-      name: req.body.name,
-     // date:
-      //time:
+        _id: req.params.id,
+        name: req.body.name,
+        // date:
+        //time:
     })
     Salon.updatedAppointment(updatedAppointment, (err, donorAppointment) => {
         if (err) {
-          res.status(500);
-          res.json({
-            data: err,
-            success: false,
-            msg: 'Failed to update appointment'
-          })
+            res.status(500);
+            res.json({
+                data: err,
+                success: false,
+                msg: 'Failed to update appointment'
+            })
         } else {
-          res.json({
-            data: salon,
-            success: true,
-            msg: 'updated appointment',
-          })
-          io.emit('update-appointment');
-    
+            res.json({
+                data: salon,
+                success: true,
+                msg: 'updated appointment',
+            })
+            io.emit('update-appointment');
+
         }
-    
-        
-      })
-    
-    
-    }) 
+
+
+    })
+
+
+})
 
 router.get('/getAll', (req, res) => {
-  
-  DonorAppointment.getAll((err, donor) => {
-    if (err) {
-      res.status(500);
-      res.json({
-        data: '',
-        success: false,
-        msg: 'Failed to get donors appointment'
-      })
-    } else {
-      res.json({
-        data: donor,
-        success: true,
-        msg: 'got donors appointment',
-      })
-    }
-  })
+
+    DonorAppointment.getAll((err, donor) => {
+        if (err) {
+            res.status(500);
+            res.json({
+                data: '',
+                success: false,
+                msg: 'Failed to get donors appointment'
+            })
+        } else {
+            res.json({
+                data: donor,
+                success: true,
+                msg: 'got donors appointment',
+            })
+        }
+    })
 
 
 })
@@ -108,44 +100,43 @@ router.get('/getAll', (req, res) => {
 router.delete('/delete/:id', (req, res) => {
     const io = req.app.get('io');
     console.log(req.params.id)
-  
-    DonorAppointment.deleteAppointment(req.params.id, (err, salon) => {
-      if (err) {
-        res.status(500);
-        res.json({
-          data: err,
-          success: false,
-          msg: 'Failed to delete the appointment'
-        })
-      } else {
-        res.json({
-          data: salon,
-          success: true,
-          msg: 'Appointment deleted',
-        })
-        io.emit('delete-appointment');
-      }
-    });
-  
-  })
 
+    DonorAppointment.deleteAppointment(req.params.id, (err, salon) => {
+        if (err) {
+            res.status(500);
+            res.json({
+                data: err,
+                success: false,
+                msg: 'Failed to delete the appointment'
+            })
+        } else {
+            res.json({
+                data: salon,
+                success: true,
+                msg: 'Appointment deleted',
+            })
+            io.emit('delete-appointment');
+        }
+    });
+
+})
 
 
 // error routes
 router.get('*', (_, res) => {
     res.status(404);
     res.send(sendResponse(undefined, false, 'path not match get requests'))
-  });
-  router.post('*', (_, res) => {
+});
+router.post('*', (_, res) => {
     res.status(404);
     res.send(sendResponse(undefined, false, 'path not match post requests'))
-  });
-  router.put('*', (_, res) => {
+});
+router.put('*', (_, res) => {
     res.status(404);
     res.send(sendResponse(undefined, false, 'path not match get requests'))
-  });
-  router.delete('*', (_, res) => {
+});
+router.delete('*', (_, res) => {
     res.status(404);
     res.send(sendResponse(undefined, false, 'path not match post requests'))
-  });
-  module.exports = router;
+});
+module.exports = router;
