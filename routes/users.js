0649f48fb71138fffa93ada7ed22403b8cca1755
tharinduvindula <tharinduvindula@gmail.com>
2020-07-14 10:08:58 +0530

    '2020071405'

--- routes/users.js
+++ routes/users.js
@@ -26,9 +26,9 @@ router.post('/signup', (req,res) => {
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress
 
-    z = ['112.135.14.4', '112.134.15.8', '112.134.56.6', '112.135.250.50', '220.247.224.55', '61.245.163.70', '112.134.235.161']
-    // ip = z[Math.floor(Math.random() * z.length)]
-    ip = '112.134.56.6'
+    // z = ['112.135.14.4', '112.134.15.8', '112.134.56.6', '112.135.250.50', '220.247.224.55', '61.245.163.70', '112.134.235.161']
+    // // ip = z[Math.floor(Math.random() * z.length)]
+    // ip = '112.134.56.6'
 
     ipapi.location((ipdata) => {
         let nuser = {
@@ -373,16 +373,17 @@ router.post('/signup', (req,res) => {
 router.post('/authenticate', (req, res) => {
     const email = req.body.email; 
     const password = req.body.password;
-    var host = req.app.get('host');
+    // var host = req.app.get('host');
+    var host = os.hostname();
     const io = req.app.get('io');
 
     var ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress
-    z = ['112.135.14.4', '112.134.15.8', '112.134.56.6', '112.135.250.50', '220.247.224.55', '61.245.163.70', '112.134.235.161']
-    // ip = z[Math.floor(Math.random() * z.length)]
-    ip = '112.134.56.6'
+    // z = ['112.135.14.4', '112.134.15.8', '112.134.56.6', '112.135.250.50', '220.247.224.55', '61.245.163.70', '112.134.235.161']
+    // // ip = z[Math.floor(Math.random() * z.length)]
+    // ip = '112.134.56.6'
    
     User.getUserBYEmail(email,(err,user)=>{
         if(err){
@@ -611,7 +612,8 @@ router.post('/emairequest',(req, res) => {
 router.post('/request', (req, res) => {
     const email = req.body.email;
     const token = req.body.token;
-    var host = req.app.get('host');
+    // var host = req.app.get('host');
+    var host = os.hostname();
     console.log(token)
 
     User.getUserBYEmail(email, (err, user) => {
@@ -924,9 +926,9 @@ router.post('/changePassword', passport.authenticate('jwt', {
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress
 
-    z = ['112.135.14.4', '112.134.15.8', '112.134.56.6', '112.135.250.50', '220.247.224.55', '61.245.163.70', '112.134.235.161']
-    // ip = z[Math.floor(Math.random() * z.length)]
-        ip = '112.134.56.6'
+    // z = ['112.135.14.4', '112.134.15.8', '112.134.56.6', '112.135.250.50', '220.247.224.55', '61.245.163.70', '112.134.235.161']
+    // // ip = z[Math.floor(Math.random() * z.length)]
+    //     ip = '112.134.56.6'
 
     User.getUserBYEmail(req.body.email, (err, user) => {
                 if (err) {
