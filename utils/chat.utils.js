0ac1b3db77b6d8780ca0d3c378d2e4170916e086
isuru1221 <isurusandaruwann1221@gmail.com>
2020-07-23 21:55:11 +0530

    changes of chat

--- utils/chat.utils.js
+++ utils/chat.utils.js
@@ -1,10 +1,16 @@
+/**
+ * message object create
+ * @param sender userId of a sender
+ * @param receiver userId of a receiver
+ * @param content <string> content of message default value is welcome
+ * @param type <string> type of a message default value set to broadcast
+ * @return {{receiver: *, sender: *, type: string, content: string}}
+ */
 module.exports.messageResponse = (
     sender,
     receiver,
-    message = 'success',
-    room,
-    type = 'broadcast',
-    success = true,
+    content = 'welcome',
+    type = 'broadcast'
 ) => {
-    return {sender, receiver, message, type, room, success}
+    return {sender, receiver, type,content}
 };
--- utils/response.utils.js
+++ utils/response.utils.js
@@ -1,3 +1,10 @@
+/**
+ * response object maker
+ * @param data if success payload
+ * @param success <boolean>
+ * @param debugMessage if error occur then error will send
+ * @return {{data: string, success: boolean, debugMessage: string}}
+ */
 module.exports.sendResponse = (data = '', success = true, debugMessage = 'success') => {
     return {data, success, debugMessage}
 };
