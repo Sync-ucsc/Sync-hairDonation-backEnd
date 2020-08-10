22d31c3f77cddc08e5df2b2c6e553238d5cfa0bc
SachinduHimash <himash1997@gmail.com>
2020-08-10 16:26:43 +0530

    path calculation

--- routes/shortestPath.route.js
+++ routes/shortestPath.route.js
@@ -6,13 +6,36 @@ const ShortestPath = require('../services/shortestPath');
 const shortestPath = new ShortestPath();
 
 
-router.post('/calculateShortestPath', async (req, res) => {
+const targetsService = require('../services/targets.service');
+const TargetsService = new targetsService();
+
+
+
+
+router.get('/sendDriverID/:email', async (req, res) => {
   try {
-    await shortestPath.calculateShortestPath();
+    let email=req.params.email;
+   
+
+    let targets = (await TargetsService.getNotCompletedTargetById(email, { status: 'NOT_COMPLETED' }))[0];
     
-    
+    let locationArray=[];
+    targets.targets.forEach(element => {
+     
+      if (element.status == 'NeedToDeliver'){
+        
+        locationArray.push(element);
+      }
+      
+    });
+    console.log(locationArray);
+    shortestPath.calculateShortestPath(locationArray);
+
   } catch (err) {
     res.status(500);
-    res.send(sendResponse(undefined, false, err));
+   
   }
 });
+
+
+module.exports = router;
--- services/shortestPath.js
+++ services/shortestPath.js
@@ -1,5 +1,6 @@
 const Graph = require('node-dijkstra');
-
+const axios=require('axios');
+const { compareSync } = require('bcryptjs');
 
 
 module.exports=class ShortestPath{
@@ -7,8 +8,51 @@ module.exports=class ShortestPath{
 constructor() {}
 
 
-calculateShortestPath(){
+
+calculateShortestPath(locations){
+console.log(locations);
+
+    const API_KEY = 'AIzaSyAkGlhRjMfmotb0UBMf8EAcmkTB6v3WEVM';
+    const a = {
+        "lat": 6.904784,
+        "lng": 79.862619,
+    }
+    const b = {
+        "lat": 6.89588,
+        "lng": 79.856985,
+    }
+
+    
+
+    let distances = [];
+
+    for (let index = 0; index < locations.length; index++) {
+        let insideArr = []
+         locations.forEach((element) => {
+           const distance = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${locations[index].lat},${locations[index].lng}&destinations=${element.lat}%2C${element.lng}&key=${API_KEY}`;
+           axios.get(distance).then((data) => {
+                insideArr.push({
+                    "id": element.salonId,
+                    "distance":data.data.rows[0].elements[0].distance
+                });
+             console.log(data.data.rows[0].elements[0].distance);
+           });
+         });
+        distances.push({"in":insideArr});
+        console.log(insideArr);
+    }
+    console.log(distances)
+
+   
+
+
 const route = new Graph();
+
+for (let index = 0; index < locations.length; index++) {
+    const element = array[index];
+    
+}
+
 
 route.addNode('A', {B: 1});
 route.addNode('B', {A: 1, C: 2, D: 4});
